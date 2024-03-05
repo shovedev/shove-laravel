7ee3ed8bf4093bf3fdc7ecfd7c42360e178ae353
Hemp <davidhemphill@users.noreply.github.com>
2024-03-05 17:58:33 -0600

    wip

--- config/shove.php
+++ config/shove.php
@@ -3,8 +3,6 @@
 return [
     'secret' => env('SHOVE_API_TOKEN', ''),
 
-    'api_url' => env('SHOVE_API_URL', 'https://shove.dev/api'),
-
     'signing_secret' => env('SHOVE_SIGNING_SECRET', ''),
 
     'default_queue' => 'default'
