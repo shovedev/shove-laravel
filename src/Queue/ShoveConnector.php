0c59490e3473cc7ba49ef916d94a7f2b0cdc1d02
Hemp <davidhemphill@users.noreply.github.com>
2024-03-25 11:18:33 -0500

    Revert "wip"
    This reverts commit 68c81c32bfc661e6fe2687d282dc19ab058fa77d.

--- src/Queue/ShoveConnector.php
+++ src/Queue/ShoveConnector.php
@@ -5,20 +5,24 @@ namespace Shove\Laravel\Queue;
 use Illuminate\Http\Request;
 use Illuminate\Queue\Connectors\ConnectorInterface;
 use Illuminate\Queue\Queue;
-use Shove\Connector\ShoveConnector as Shove;
+use Illuminate\Support\Facades\Config;
+use Shove\Connector\ShoveConnector as ShoveHttpClient;
 
 class ShoveConnector implements ConnectorInterface
 {
-    public function __construct(protected Request $request, protected Shove $client)
+    public function __construct(protected Request $request)
     {
     }
 
     public function connect(array $config): Queue
     {
         return new ShoveQueue(
-            $this->client,
+            new ShoveHttpClient(
+                token: Config::get('shove.secret', ''),
+                baseUrl: Config::get('shove.api_url')
+            ),
             $this->request,
-            $config['default_queue']
+            Config::get('shove.default_queue')
         );
     }
 }
